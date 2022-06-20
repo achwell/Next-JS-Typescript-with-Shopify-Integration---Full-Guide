1275f5b99a9305dab20bf6b64d5199d051789143
Axel Wulff SÃther <axelwulff@mac.com>
2022-06-20 06:16:17 +0200

    Etter Section 11: Reusable Configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const {withFrameworkConfig} = require("./framework/common/config")
 
-module.exports = nextConfig
+module.exports = withFrameworkConfig({
+    framework: {
+        name: "shopify_local"
+    },
+    i18n: {
+        locales: ["no-NB", "en-US", "es"],
+        defaultLocale: "no-NB"
+    }
+})
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import type {InferGetStaticPropsType} from "next"
-import getAllProducts from "@framework/product/get-all-products"
+import getAllproducts from "@framework/product/get-all-products"
+import {getConfig} from "@framework/api/config"
 
 export async function getStaticProps() {
-    const products = await getAllProducts()
+    const config = getConfig()
+    const products = await getAllproducts(config)
+
     return {
         props: {
             products
