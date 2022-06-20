70be4aca7c0faf63a12440d20c90b0ee4bf24c85
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-06-20 08:19:43 +0200

    Etter Section 13: Tailwind & PostCSS

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react"
+import s from "./Layout.module.css"
 
 const Layout: FC = ({children}) => {
     return (
-        <div className="layout">
-            { children }
+        <div className={s.root}>
+            <main className="fit">
+                { children }
+            </main>
         </div>
     )
 }
